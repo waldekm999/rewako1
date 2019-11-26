6c99a062d65f40882e2f606eb45076bd2177cf38
waldekm999 <waldekm999@hotmail.com>
2019-11-26 21:10:17 +0100

    start point - after npm install anfd first build prod with sample images nad sources

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rewako1
 Pierwszy projekt dla Rewako - w oparciu o node (npm, webpack) - Frontend
+
---
+++ src/tools/creator.js
@@ -0,0 +1,15 @@
+export default class {
+  constructor() {
+    this.element = document.createElement('div');
+    this.element.style.height = "100px";
+    document.body.appendChild(this.element);
+  }
+  color = "cadetblue";
+
+  addBgc(color = this.color) {
+    this.element.style.backgroundColor = color;
+  }
+  showColor = () => {
+    console.log(this.element.style.backgroundColor);
+  }
+}
